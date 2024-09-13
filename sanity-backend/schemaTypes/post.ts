5062d94a3d39bc7088340b793e9463d0a913f0ae
regis066 <mugisharegis72@gmail.com>
2024-09-13 15:37:11 +0200

    Create Post schema

---
+++ sanity-backend/schemaTypes/post.ts
@@ -0,0 +1,50 @@
+import {defineType, defineField} from 'sanity'
+
+export const post = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'caption',
+      title: 'Caption',
+      type: 'string',
+    }),
+    defineField({
+      name: 'video',
+      title: 'Video',
+      type: 'file',
+    }),
+    defineField({
+      name: 'userId',
+      title: 'UserId',
+      type: 'string',
+    }),
+    defineField({
+      name: 'likes',
+      title: 'Likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+    }),
+    defineField({
+      name: 'comments',
+      title: 'Comments',
+      type: 'array',
+      of: [
+        {
+          type: 'comment',
+        },
+      ],
+    }),
+      defineField({
+          name: "topic",
+          title: "Topic",
+          type: "string"
+    })
+  ],
+})
