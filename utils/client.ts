76ac3ad75b27944dc2bde24b4c471ac0dd24aa7f
regis066 <mugisharegis72@gmail.com>
2024-09-25 07:39:41 +0200

    Configure @sanity/client

---
+++ utils/client.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@sanity/client";
+
+
+export const client = createClient({
+  projectId: "efwp5woh",
+  dataset: "production",
+  useCdn: false, // set to `false` to bypass the edge cache
+  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // Only if you want to update content with the client
+});
