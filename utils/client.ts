21ec648cdeea922cbef67b960b9a20e8b6eedfc4
regis066 <mugisharegis72@gmail.com>
2024-09-25 10:38:23 +0200

    Change apiVersion to the one used in sanity vision

--- utils/client.ts
+++ utils/client.ts
@@ -5,6 +5,6 @@ export const client = createClient({
   projectId: "efwp5woh",
   dataset: "production",
   useCdn: false, // set to `false` to bypass the edge cache
-  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
+  apiVersion: "2022-03-07", // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, // Only if you want to update content with the client
 });
