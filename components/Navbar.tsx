43db6ff5117bc12b121478d0a38deb070a961308
regis066 <mugisharegis72@gmail.com>
2024-09-26 21:09:04 +0200

    feat: Add Google OAuth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -10,17 +10,31 @@ import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-b-gray-200 py-2 px-4">
       <Link href="/">
         <div className="w-[100px] md:w-[130px]">
-          <Image
-            src={Logo}
-            className="cursor-pointer"
-            alt="TikTik"
-          />
+          <Image src={Logo} className="cursor-pointer" alt="TikTik" />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+              }}
+              
+              onError={() => {
+                console.log("Login failed");
+              }}
+              useOneTap
+          />
+        )}
+      </div>
     </div>
   );
 };
