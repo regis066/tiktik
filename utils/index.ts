78f4d7a02fb85da14e190b99eb461c62f05e49e7
regis066 <mugisharegis72@gmail.com>
2024-09-30 15:31:34 +0200

    Create createOrGetUser function

---
+++ utils/index.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const createOrGetUser = async (response: any) => {
+
+  const token = response.credential;
+  const decoded: { name: string; picture: string; sub: string } =
+    jwtDecode(token);
+  
+  const { name, picture, sub } = decoded;
+
+  const user = {
+    _id: sub,
+    _type: "user",
+    userName: name,
+    image: picture,
+  }
+
+  await axios
+    .post(`http://localhost:3000/api/auth`, user)
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+;
+
+  // console.log(decoded);
+};
