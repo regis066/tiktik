f13b028a79a33f39867c00ce3583ba1e7bdbf9c4
regis066 <mugisharegis72@gmail.com>
2024-10-01 15:22:37 +0200

    feat: Add google logout functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,11 @@ import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "@/utils";
+import useAuthStore from "@/store/authStore";
 
 const Navbar = () => {
-  const user = false;
+  const { userProfile, addUser, removeUser } = useAuthStore();
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-b-gray-200 py-2 px-4">
       <Link href="/">
@@ -20,18 +22,45 @@ const Navbar = () => {
       </Link>
       <div>SEARCH</div>
       <div>
-        {user ? (
-          <div>Logged In</div>
+        {userProfile ? (
+          <div className="flex gap-5 md:gap-10 items-center">
+            <Link href="/upload">
+              <button className="border-2  p-2  md:px-4 text-md  font-semibold flex items-center gap-2">
+                <IoMdAdd className="text-xl" /> {` `}
+                <span className="hidden md:block">Upload</span>
+              </button>
+            </Link>
+
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full"
+                    src={userProfile.image}
+                    alt="profile photo"
+                  />
+                </>
+              </Link>
+            )}
+
+            <button type="button" className="px-2" onClick={() => {
+              googleLogout();
+              removeUser();
+            }} >
+              <AiOutlineLogout color="red" className="text-3xl" />
+            </button>
+          </div>
         ) : (
           <GoogleLogin
             onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
-              }}
-              
-              onError={() => {
-                console.log("Login failed");
-              }}
-              useOneTap
+              createOrGetUser(credentialResponse, addUser);
+            }}
+            onError={() => {
+              console.log("Login failed");
+            }}
+            useOneTap
           />
         )}
       </div>
