ed7a872909ff7769a6affc73b6d4038e4a850c01
regis066 <mugisharegis72@gmail.com>
2024-10-03 21:03:23 +0200

    Fetch all video posts and render them using VideoCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { NoResults } from "@/components/NoResults";
+import VideoCard from "@/components/VideoCard";
+import { Video } from "@/types";
+import axios from "axios";
+
+export default async function Home() {
+
+
+  const { data } = await axios.get("http://localhost:3000/api/post");
+
+  const videos = data;
+  
   return (
-    <div className="text-3xl font-bold">
-      TikTik
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+      ): <NoResults text={'No Videos'} />}
     </div>
   );
 }
