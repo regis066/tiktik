c856f54b58f8d89d0bdfd5c5bea10c0e085ddf14
regis066 <mugisharegis72@gmail.com>
2024-10-03 20:57:05 +0200

    Add new parameter(addUser) to createOrGetUser function

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ import { jwtDecode } from "jwt-decode";
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const createOrGetUser = async (response: any) => {
+export const createOrGetUser = async (response: any, addUser:any) => {
 
   const token = response.credential;
   const decoded: { name: string; picture: string; sub: string } =
@@ -17,6 +17,8 @@ export const createOrGetUser = async (response: any) => {
     userName: name,
     image: picture,
   }
+
+  addUser(user);
 
   await axios
     .post(`http://localhost:3000/api/auth`, user)
