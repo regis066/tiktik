852edba26547e0342b0fcf3720b664fbd8bcf64a
regis066 <mugisharegis72@gmail.com>
2024-10-06 14:43:49 +0200

    feat: Add POST request handler function and also update GET

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -2,9 +2,34 @@ import { client } from "@/utils/client";
 import { allPostsQuery } from "@/utils/queries";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, res: Response) {
-  const query = allPostsQuery();
-  const data = await client.fetch(query);
 
-  return NextResponse.json(data);
+export async function GET() {
+  const query = allPostsQuery();
+
+  try {
+    const data = await client.fetch(query);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch posts" },
+      { status: 500 }
+    );
+  }
+}
+
+
+export async function POST(req: Request) {
+  const document = await req.json();
+
+  try {
+    await client.create(document);
+    return NextResponse.json({ message: "Video created" }, { status: 201 })
+  } catch (error) {
+    console.error("Error creating document:", error);
+    return NextResponse.json(
+      { error: "Failed to create video" },
+      { status: 500 },
+    );
+  }
 }
